b60216814ceb5030e1e38729aee283a8b118583a
Anish <6275anioshroy@gmail.com>
2020-09-08 19:26:11 +0530

    Removed frontend named folder

--- frontEnd/assets/js/searchUserHandler3.js
+++
@@ -1,7 +0,0 @@
-function sayHi(hiId, hiImage, hiName){
-    var curTime = new Date();
-    sendToBackend('hi', hiId, curTime);
-    insert(lastChat++, hiImage, hiName, userId, hiId, '1', 'hi', curTime);
-    ShowToast(hiName);
-    sendToReciever(Ses_img, username, hiId, userId, '1', 'hi', curTime); 
-}
